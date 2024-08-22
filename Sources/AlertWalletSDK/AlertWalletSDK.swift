d79c3f4a6e550bd8c42e9305bd59a2a6b6c305b8
Soori <soori@sudheeksha.co>
2024-08-22 22:10:40 +0530

    asdasd

--- Sources/AlertWalletSDK/AlertWalletSDK.swift
+++ Sources/AlertWalletSDK/AlertWalletSDK.swift
@@ -2,16 +2,10 @@ import PassKit
 
 public class AlertWalletSDK {
 
-    var watchDetector: AppleWatchDetector
-
     public init(delegate : AlertWalletControllerDelegate){
         AppleWatchDetector.shared.delegate = delegate
         AppleWatchDetector.shared.initialize()
+
     }
-
-
-
-
-
 }
 
