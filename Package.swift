637e8261e2db6e22b9bb14749cba238b79fc876e
Soori <soori@sudheeksha.co>
2024-08-22 12:51:30 +0530

    Refactor to AlertWallet

--- Package.swift
+++ Package.swift
@@ -4,20 +4,20 @@
 import PackageDescription
 
 let package = Package(
-    name: "AEWalletSDK",
+    name: "AlertWalletSDK",
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "AEWalletSDK",
-            targets: ["AEWalletSDK"]),
+            name: "AlertWalletSDK",
+            targets: ["AlertWalletSDK"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "AEWalletSDK"),
+            name: "AlertWalletSDK"),
         .testTarget(
-            name: "AEWalletSDKTests",
-            dependencies: ["AEWalletSDK"]),
+            name: "AlertWalletSDKTests",
+            dependencies: ["AlertWalletSDK"]),
     ]
 )
