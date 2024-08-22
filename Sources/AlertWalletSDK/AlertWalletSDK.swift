9b217bb8ca9a0b57a06d07e7e52e38910967af9d
Soori <soori@sudheeksha.co>
2024-08-22 18:24:34 +0530

    removed class

--- Sources/AlertWalletSDK/AlertWalletSDK.swift
+++ Sources/AlertWalletSDK/AlertWalletSDK.swift
@@ -2,16 +2,7 @@ import PassKit
 
 public class AlertWalletSDK {
 
-    var watchDetector: AppleWatchDetector
 
-    public init(){
-        watchDetector = AppleWatchDetector()
-    }
-
-    public func isWatchPaired() -> Bool{
-        watchDetector.detect()
-        return watchDetector.watchPaired
-    }
 
 }
 
