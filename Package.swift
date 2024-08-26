723b1b696ad7ad67440f53a0ed74b016560bb42c
Soori <soori@sudheeksha.co>
2024-08-26 18:16:00 +0530

    images test again

--- Package.swift
+++ Package.swift
@@ -18,7 +18,11 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "AlertWalletSDK"),
+            name: "AlertWalletSDK",
+            dependencies: [],
+            resources: [
+                .process("Resources") // This includes all resources in the Resources directory
+            ]),
         .testTarget(
             name: "AlertWalletSDKTests",
             dependencies: ["AlertWalletSDK"]),
