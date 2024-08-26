30979758021248fc8b571b097b40e5dcabbfa15e
Soori <soori@sudheeksha.co>
2024-08-26 19:45:47 +0530

    Image Helper

---
+++ Sources/AlertWalletSDK/Helper/ImageProvider.swift
@@ -0,0 +1,19 @@
+//
+//  File.swift
+//  
+//
+//  Created by Reddy on 26/08/24.
+//
+
+import Foundation
+import UIKit
+
+public class ImageProvider {
+
+    public init() {}
+
+    public func image(named imageName: String) -> UIImage? {
+        return UIImage(named: imageName, in: Bundle.module, compatibleWith: nil)
+    }
+}
+
