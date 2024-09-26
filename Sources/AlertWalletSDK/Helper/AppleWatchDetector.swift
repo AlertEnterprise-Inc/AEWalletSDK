26131745a47391d4b30931d7d58fc59841525174
akshgaur <aksh.gaur@alertenterprise.com>
2024-09-26 17:10:04 +0530

    update

--- Sources/AlertWalletSDK/Helper/AppleWatchDetector.swift
+++ Sources/AlertWalletSDK/Helper/AppleWatchDetector.swift
@@ -1,82 +1,44 @@
 //
 //  AppleWatchDetector.swift
+//  AEWallet
 //
-//
-//  Created by Reddy on 22/08/24.
+//  Created by aksh gaur on 01/11/23.
 //
 
 import WatchConnectivity
-final class AppleWatchDetector: NSObject{
-    
-    public static let shared = AppleWatchDetector()
+import os
+
+final class AppleWatchDetector: NSObject {
+
+    private static let logger = Logger(
+        subsystem: Bundle.main.bundleIdentifier!,
+        category: String(describing: AppleWatchDetector.self)
+    )
+
+    static let shared = AppleWatchDetector()
+    var session: WCSession?  = nil
+    var watchPaired = false
     public weak var delegate: AlertWalletControllerDelegate?
-    private var session : WCSession? = nil
-    
-    private override init() {
-//        super.init()
-//        NSLog("watch init");
-//        if WCSession.isSupported() {
-//            WCSession.default.delegate = self
-//            WCSession.default.activate()
-//        }else{
-//            self.session = nil
-//        }
-    }
-    
+
     func detect() {
         if(WCSession.isSupported()){
             session = WCSession.default
             session?.delegate = self
             session?.activate()
         }else{
-            NSLog("AppleWatchDetector :: detect()  WCSession.isSupported()   is false  " )
+            Self.logger.error("AppleWatchDetector :: detect()  WCSession.isSupported()   is false  " )
         }
     }
-    
-//    func session(_ session: WCSession, activationDidCompleteWith activationState: WCSessionActivationState, error: (any Error)?) {
-//        self.session = session
-//        let isPaired  = self.session!.isPaired
-//    }
-    
-//    func sessionDidBecomeInactive(_ session: WCSession) {
-//    }
-//    
-//    func sessionDidDeactivate(_ session: WCSession) {
-//        WCSession.default.activate()
-//    }
-    
-    public func checkIfWatchIsPaired() {
-        if(self.session != nil){
-            let isPaired = self.session!.isPaired
-            delegate?.AlertWalletUIViewController(AlertWalletController.shared, isWatchPaired: isPaired)
-        }else if(self.session == nil){
-            if WCSession.isSupported() {
-                let isPaired = WCSession.default.isPaired
-                delegate?.AlertWalletUIViewController(AlertWalletController.shared, isWatchPaired: isPaired)
-            }
-        }else{
-            delegate?.AlertWalletUIViewController(AlertWalletController.shared, isWatchPaired: false)
-        }
-    }
-    public func initialize() {
-//        checkIfWatchIsPaired()
-        detect()
-        NSLog("watch detect");
-    }
-    
 }
 
 extension AppleWatchDetector: WCSessionDelegate {
 
     func session(_ session: WCSession, activationDidCompleteWith activationState: WCSessionActivationState, error: Error?) {
-//        self.watchPaired = session.isPaired
-        self.session = session
-        let isPaired  = self.session!.isPaired
-        delegate?.AlertWalletUIViewController(AlertWalletController.shared, isWatchPaired: isPaired)
+        self.watchPaired = session.isPaired
     }
 
     func session(_ session: WCSession, didReceiveApplicationContext applicationContext: [String : Any]) {
-        NSLog("AppleWatchDetector :: session  Received application context: \(applicationContext)  " )
+        Self.logger.debug("AppleWatchDetector :: session  Received application context: \(applicationContext)  " )
     }
 
     func sessionDidDeactivate(_ session: WCSession) {
@@ -85,4 +47,3 @@ extension AppleWatchDetector: WCSessionDelegate {
     func sessionDidBecomeInactive(_ session: WCSession) {
     }
 }
-
